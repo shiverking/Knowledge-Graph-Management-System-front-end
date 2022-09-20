db3653dbdd3559089c132f158a7aadda6c7e3ff5
Jiawen9 <86347240+Jiawen9@users.noreply.github.com>
2022-09-20 11:09:11 +0800

    9-20修改
    前端小小改动

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,7 +68,6 @@
                   <router-link to="/kg/completion/GetTriples" class="routerlink" ><el-menu-item index="4-2-1">获取三元组</el-menu-item></router-link>
                   <router-link to="/kg/completion/LinkPrediction" class="routerlink"><el-menu-item index="4-2-2">链接预测</el-menu-item></router-link>
                   <router-link to="/kg/completion/AutomatedCompletion" class="routerlink"><el-menu-item index="4-2-3">自动化补全</el-menu-item></router-link>
-                  <router-link to="/kg/completion/HandleConflicts" class="routerlink"><el-menu-item index="4-2-4">冲突处理</el-menu-item></router-link>
                   <router-link to="/kg/completion/ViewHistory" class="routerlink"><el-menu-item index="4-2-5">历史补全</el-menu-item></router-link>
                 </el-submenu>
               </el-submenu>
