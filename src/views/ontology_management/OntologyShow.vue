c8d954469b54bc4523ab771cda0142c5c2cfeccf
田若星 <35066603+shiverking@users.noreply.github.com>
2022-10-31 15:35:48 +0800

    修改图谱融合部分界面功能，修改图标

--- src/views/ontology_management/OntologyShow.vue
+++ src/views/ontology_management/OntologyShow.vue
@@ -4,7 +4,7 @@
       <span class="desc">本体结构展示</span>
       <svg id="show_ontology_in_svg" width="100%" height="100%"></svg>
     </div>
-    <el-card class="class_info">
+    <el-card class="class_info" style="margin-top: 0px;margin-left: 0px;position: absolute;right: 11px;height: auto">
       <el-descriptions title="节点详细信息" direction="vertical" :column="2" border>
         <el-descriptions-item label="节点名">Country</el-descriptions-item>
         <el-descriptions-item label="父节点">GIS</el-descriptions-item>
@@ -13,107 +13,124 @@
         <el-descriptions-item label="数据库">FKFD</el-descriptions-item>
         <el-descriptions-item label="映射数据表">t_country</el-descriptions-item>
       </el-descriptions>
+      <el-row :gutter="20">
+        <el-col :span="6">
+          <div class="grid-content bg-purple">
+          <el-button id="update_class_button" @click="updateClassButton = true" type="primary" class="update_button">
+            修改节点
+          </el-button>
+        </div>
+        </el-col>
+        <el-col :span="6"><div class="grid-content bg-purple">
+          <el-button id="update_relation_button" @click="updateRelationButton = true" type="primary" class="update_button">
+            修改关系
+          </el-button>
+        </div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple">
+          <el-button id="add_class_button" @click="addClassButton = true" type="primary" class="update_button">
+            添加节点
+          </el-button>
+        </div></el-col>
+        <el-col :span="6"><div class="grid-content bg-purple">
+          <el-button id="add_relation_button" @click="addRelationButton = true" type="primary" class="update_button">
+            添加关系
+          </el-button>
+        </div></el-col>
+      </el-row>
+
+      <el-drawer title="请输入需要修改的节点的信息" :visible.sync="updateClassButton" :with-header="true">
+        <el-form ref="form" :model="addClass" label-width="80px">
+          <el-form-item label="节点名称">
+            <el-input v-model="addClass.name"></el-input>
+          </el-form-item>
+          <el-form-item label="备注信息">
+            <el-input v-model="addClass.desc" placeholder="请修改备注"></el-input>
+          </el-form-item>
+          <el-form-item label="父节点">
+            <el-select v-model="addClass.father" placeholder="请修改父节点">
+              <el-option label="无" value=""></el-option>
+              <el-option label="父节点一" value="Country"></el-option>
+              <el-option label="父节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="checkUpdateClass" size="small">确认修改</el-button>
+            <el-button size="small">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-drawer>
+
+      <el-drawer title="请输入需要修改的关系的信息" :visible.sync="updateRelationButton" :with-header="true">
+        <el-form ref="form" :model="addRelation" label-width="80px">
+          <el-form-item label="关系名称">
+            <el-input v-model="addRelation.name"></el-input>
+          </el-form-item>
+          <el-form-item label="头节点">
+            <el-select v-model="addRelation.classList" placeholder="请选择头节点">
+              <el-option label="头节点一" value="Country"></el-option>
+              <el-option label="头节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="尾节点">
+            <el-select v-model="addRelation.classList" placeholder="请选择尾节点">
+              <el-option label="尾节点一" value="Country"></el-option>
+              <el-option label="尾节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="checkUpdateRelation" size="small">确认修改</el-button>
+            <el-button size="small">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-drawer>
+
+      <el-drawer title="请输入需要添加的节点的信息" :visible.sync="addClassButton" :with-header="true">
+        <el-form ref="form" :model="addClass" label-width="80px">
+          <el-form-item label="节点名称">
+            <el-input v-model="addClass.name"></el-input>
+          </el-form-item>
+          <el-form-item label="备注信息">
+            <el-input v-model="addClass.desc" placeholder="请输入备注"></el-input>
+          </el-form-item>
+          <el-form-item label="父节点">
+            <el-select v-model="addClass.father" placeholder="请选择父节点">
+              <el-option label="无" value=""></el-option>
+              <el-option label="父节点一" value="Country"></el-option>
+              <el-option label="父节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="checkAddClass" size="small">确认添加</el-button>
+            <el-button size="small">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-drawer>
+
+      <el-drawer title="请输入需要添加的关系的信息" :visible.sync="addRelationButton" :with-header="true">
+        <el-form ref="form" :model="addRelation" label-width="80px">
+          <el-form-item label="关系名称">
+            <el-input v-model="addRelation.name"></el-input>
+          </el-form-item>
+          <el-form-item label="头节点">
+            <el-select v-model="addRelation.classList" placeholder="请选择头节点">
+              <el-option label="头节点一" value="Country"></el-option>
+              <el-option label="头节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="尾节点">
+            <el-select v-model="addRelation.classList" placeholder="请选择尾节点">
+              <el-option label="尾节点一" value="Country"></el-option>
+              <el-option label="尾节点二" value="Sky"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="checkAddRelation" size="small">确认添加</el-button>
+            <el-button size="small">取消</el-button>
+          </el-form-item>
+        </el-form>
+      </el-drawer>
     </el-card>
-    <el-button id="update_class_button" @click="updateClassButton = true" type="primary" style="margin-left: 16px;">
-      修改节点
-    </el-button>
-    <el-drawer title="请输入需要修改的节点的信息" :visible.sync="updateClassButton" :with-header="true">
-      <el-form ref="form" :model="addClass" label-width="80px">
-        <el-form-item label="节点名称">
-          <el-input v-model="addClass.name"></el-input>
-        </el-form-item>
-        <el-form-item label="备注信息">
-          <el-input v-model="addClass.desc" placeholder="请修改备注"></el-input>
-        </el-form-item>
-        <el-form-item label="父节点">
-          <el-select v-model="addClass.father" placeholder="请修改父节点">
-            <el-option label="无" value=""></el-option>
-            <el-option label="父节点一" value="Country"></el-option>
-            <el-option label="父节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="checkUpdateClass" size="small">确认修改</el-button>
-          <el-button size="small">取消</el-button>
-        </el-form-item>
-      </el-form>
-    </el-drawer>
-    <el-button id="update_relation_button" @click="updateRelationButton = true" type="primary" style="margin-left: 16px;">
-      修改关系
-    </el-button>
-    <el-drawer title="请输入需要修改的关系的信息" :visible.sync="updateRelationButton" :with-header="true">
-      <el-form ref="form" :model="addRelation" label-width="80px">
-        <el-form-item label="关系名称">
-          <el-input v-model="addRelation.name"></el-input>
-        </el-form-item>
-        <el-form-item label="头节点">
-          <el-select v-model="addRelation.classList" placeholder="请选择头节点">
-            <el-option label="头节点一" value="Country"></el-option>
-            <el-option label="头节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="尾节点">
-          <el-select v-model="addRelation.classList" placeholder="请选择尾节点">
-            <el-option label="尾节点一" value="Country"></el-option>
-            <el-option label="尾节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="checkUpdateRelation" size="small">确认修改</el-button>
-          <el-button size="small">取消</el-button>
-        </el-form-item>
-      </el-form>
-    </el-drawer>
-    <el-button id="add_class_button" @click="addClassButton = true" type="primary" style="margin-left: 16px;">
-      添加节点
-    </el-button>
-    <el-drawer title="请输入需要添加的节点的信息" :visible.sync="addClassButton" :with-header="true">
-      <el-form ref="form" :model="addClass" label-width="80px">
-        <el-form-item label="节点名称">
-          <el-input v-model="addClass.name"></el-input>
-        </el-form-item>
-        <el-form-item label="备注信息">
-          <el-input v-model="addClass.desc" placeholder="请输入备注"></el-input>
-        </el-form-item>
-        <el-form-item label="父节点">
-          <el-select v-model="addClass.father" placeholder="请选择父节点">
-            <el-option label="无" value=""></el-option>
-            <el-option label="父节点一" value="Country"></el-option>
-            <el-option label="父节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="checkAddClass" size="small">确认添加</el-button>
-          <el-button size="small">取消</el-button>
-        </el-form-item>
-      </el-form>
-    </el-drawer>
-    <el-button id="add_relation_button" @click="addRelationButton = true" type="primary" style="margin-left: 16px;">
-      添加关系
-    </el-button>
-    <el-drawer title="请输入需要添加的关系的信息" :visible.sync="addRelationButton" :with-header="true">
-      <el-form ref="form" :model="addRelation" label-width="80px">
-        <el-form-item label="关系名称">
-          <el-input v-model="addRelation.name"></el-input>
-        </el-form-item>
-        <el-form-item label="头节点">
-          <el-select v-model="addRelation.classList" placeholder="请选择头节点">
-            <el-option label="头节点一" value="Country"></el-option>
-            <el-option label="头节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="尾节点">
-          <el-select v-model="addRelation.classList" placeholder="请选择尾节点">
-            <el-option label="尾节点一" value="Country"></el-option>
-            <el-option label="尾节点二" value="Sky"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="checkAddRelation" size="small">确认添加</el-button>
-          <el-button size="small">取消</el-button>
-        </el-form-item>
-      </el-form>
-    </el-drawer>
+
   </div>
 </template>
 
@@ -124,10 +141,7 @@ import * as jquery from '../../plugins/jquery.min.js'
 import Panzoom from '@panzoom/panzoom';
 export default {
   name: 'OntologyShow',
-
   data() {
-
-
     return{
       dataset:{
         name: "Thing",
@@ -345,6 +359,7 @@ export default {
                   bottom: '1%',
                   right: '20%',
                   symbolSize: 7,
+                  roam: true,
                   label: {
                     position: 'left',
                     verticalAlign: 'middle',
@@ -454,25 +469,10 @@ export default {
   font-size: 16px;
   font-weight: 700;
 }
-#update_class_button{
-  margin-left: 150px;
+.update_button{
+  /*margin-left: 150px;*/
   margin-top: 10px;
-  float: right;
-}
-#update_relation_button{
-  margin-right: 150px;
-  margin-top: 10px;
-  float: right;
-}
-#add_class_button{
-  margin-left: 150px;
-  margin-top: 10px;
-  float: right;
-}
-#add_relation_button{
-  margin-right: 150px;
-  margin-top: 10px;
-  float: right;
+  /*float: right;*/
 }
 .class_info {
   float: left;
